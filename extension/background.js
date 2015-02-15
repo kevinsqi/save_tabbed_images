0ac05910d563d333d14088dff28469e782fbf31a
Kevin Qi <iqnivek@gmail.com>
2015-02-15 13:10:33 -0500

    Switch from tabs to 2 space indentation

--- extension/background.js
+++ extension/background.js
@@ -1,79 +1,79 @@
 var tabsWithImages = {};
 
 var imageMimeTypes = {
-	"image/jpeg": true,
-	"image/png":  true,
-	"image/gif":  true,
-	"image/webp": true
+  "image/jpeg": true,
+  "image/png":  true,
+  "image/gif":  true,
+  "image/webp": true
 };
 
 var imageExtensions = {
-	"jpg":  true,
-	"jpeg": true,
-	"png":  true,
-	"gif":  true,
-	"webp": true
+  "jpg":  true,
+  "jpeg": true,
+  "png":  true,
+  "gif":  true,
+  "webp": true
 };
 
 // Track which tabs are images based on MIME type
 chrome.webRequest.onHeadersReceived.addListener(function(details) {
-	if (details.tabId !== -1) {
-		var headerValue = getContentTypeHeader(details.responseHeaders);
+  if (details.tabId !== -1) {
+    var headerValue = getContentTypeHeader(details.responseHeaders);
 
-		// If false, we know tab is not an image, and skip fallback check.
-		tabsWithImages[details.tabId] = imageMimeTypes[headerValue] || false;
-	}
+    // If false, we know tab is not an image, and skip fallback check.
+    tabsWithImages[details.tabId] = imageMimeTypes[headerValue] || false;
+  }
 }, {
-	urls: ['*://*/*'],
-	types: ['main_frame']
+  urls: ['*://*/*'],
+  types: ['main_frame']
 }, ['responseHeaders']);
 
 function getContentTypeHeader(headers) {
-	var contentTypeHeader;
-	var contentTypeHeaderValue;
+  var contentTypeHeader;
+  var contentTypeHeaderValue;
 
-    for (var i = 0; i < headers.length; ++i) {
-        var header = headers[i].toLowerCase();
-        if (header.name === 'content-type') {
-            contentTypeHeader = header;
-            break;
-        }
+  for (var i = 0; i < headers.length; ++i) {
+    var header = headers[i].toLowerCase();
+    if (header.name === 'content-type') {
+      contentTypeHeader = header;
+      break;
     }
+  }
 
-	// If header is set, use its value. Otherwise, use undefined.
-    contentTypeHeaderValue = contentTypeHeader && contentTypeHeader.value.split(';', 1)[0];
+  // If header is set, use its value. Otherwise, use undefined.
+  contentTypeHeaderValue = contentTypeHeader && contentTypeHeader.value.split(';', 1)[0];
 
-	// Normalize case
-	if (contentTypeHeaderValue) {
-		contentTypeHeaderValue = contentTypeHeaderValue.toLowerCase();
-	}
+  // Normalize case
+  if (contentTypeHeaderValue) {
+    contentTypeHeaderValue = contentTypeHeaderValue.toLowerCase();
+  }
 
-	return contentTypeHeaderValue;
+  return contentTypeHeaderValue;
 }
 
 // Given array of tabs, return URLs of those which are images
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if (request.type === "checktabs") {
-			var tabsToReturn = [];
-			var tabs = request.tabs;
-			for (var i = 0; i < tabs.length; i++) {
-				var tab = tabs[i];
-				var tabObj = {id: tab.id, url: tab.url};
-				if (tabsWithImages[tab.id] === true) {
-					tabsToReturn.push(tabObj);
-				} else if (tabsWithImages[tab.id] === false) {
-					// Tab is not an image, do nothing.
-				} else if (match = tab.url.match(/.+\.([^?]+)(\?|$)/)) {  // regex captures the URL's file extension
-					// If we didn't get it from MIME checking (i.e. if extension had been disabled or just installed), check the file extension as a fallback.
-					// Can create false positives, but better than overlooking files.
-					var ext = match[1].toLowerCase();
-					if (imageExtensions[ext]) {
-						tabsToReturn.push(tabObj);
-					}
-				}
-			}
-			sendResponse({tabs: tabsToReturn});
-		}
-	}
+  function(request, sender, sendResponse) {
+    if (request.type === "checktabs") {
+      var tabsToReturn = [];
+      var tabs = request.tabs;
+      for (var i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+        var tabObj = {id: tab.id, url: tab.url};
+        if (tabsWithImages[tab.id] === true) {
+          tabsToReturn.push(tabObj);
+        } else if (tabsWithImages[tab.id] === false) {
+          // Tab is not an image, do nothing.
+        } else if (match = tab.url.match(/.+\.([^?]+)(\?|$)/)) {  // regex captures the URL's file extension
+          // If we didn't get it from MIME checking (i.e. if extension had been disabled or just installed), check the file extension as a fallback.
+          // Can create false positives, but better than overlooking files.
+          var ext = match[1].toLowerCase();
+          if (imageExtensions[ext]) {
+            tabsToReturn.push(tabObj);
+          }
+        }
+      }
+      sendResponse({tabs: tabsToReturn});
+    }
+  }
 );
--- extension/popup.js
+++ extension/popup.js
@@ -2,118 +2,118 @@
 var tabDownloadStatuses = {};
 
 function pluralize(count, str) {
-	if (count === 1) {
-		return count + ' ' + str;
-	} else {
-		return count + ' ' + str + 's';
-	}
+  if (count === 1) {
+    return count + ' ' + str;
+  } else {
+    return count + ' ' + str + 's';
+  }
 }
 
 function getTabsWithImages(callback) {
-	chrome.tabs.query(
-		{currentWindow: true},
-		function(tabs) {
-			// Query background process for which tabs are images
-			chrome.runtime.sendMessage({type: "checktabs", tabs: tabs}, function(response) {
-				callback(response.tabs);
-			});
-		}
-	);
+  chrome.tabs.query(
+    {currentWindow: true},
+    function(tabs) {
+      // Query background process for which tabs are images
+      chrome.runtime.sendMessage({type: "checktabs", tabs: tabs}, function(response) {
+        callback(response.tabs);
+      });
+    }
+  );
 }
 
 // List image URLs in popup
 function showImageUrls() {
-	getTabsWithImages(function(tabs) {
-		// Add URLs to list
-		var links = $('#links');
-		for (var i = 0; i < tabs.length; i++) {
-			var url = tabs[i].url;
-			var link = $('<a></a>').text(url).prop('href', url);
-			var listItem = $('<li></li>').append(link);
-			links.append(listItem);
-		}
+  getTabsWithImages(function(tabs) {
+    // Add URLs to list
+    var links = $('#links');
+    for (var i = 0; i < tabs.length; i++) {
+      var url = tabs[i].url;
+      var link = $('<a></a>').text(url).prop('href', url);
+      var listItem = $('<li></li>').append(link);
+      links.append(listItem);
+    }
 
-		// Customize controls
-		var message = $('#message');
-		if (tabs.length > 0) {
-			message.text(pluralize(tabs.length, "image") + " in current window:");
-			$('#download').text('Download ' + pluralize(tabs.length, "image"));
-		} else {
-			// No images are loaded
-			message.text("No images opened as tabs in current window.");
-			$('#download').hide();
-			$('#dismiss').show();
-		}
-	});
+    // Customize controls
+    var message = $('#message');
+    if (tabs.length > 0) {
+      message.text(pluralize(tabs.length, "image") + " in current window:");
+      $('#download').text('Download ' + pluralize(tabs.length, "image"));
+    } else {
+      // No images are loaded
+      message.text("No images opened as tabs in current window.");
+      $('#download').hide();
+      $('#dismiss').show();
+    }
+  });
 }
 
 function downloadImageUrls() {
-	$('#download').prop('disabled', 'disabled');
-	$('#close-tabs').show();
+  $('#download').prop('disabled', 'disabled');
+  $('#close-tabs').show();
 
-	getTabsWithImages(function(tabs) {
-		for (var i = 0; i < tabs.length; i++) {
-			(function() {
-				var url = tabs[i].url;
-				var tabId = tabs[i].id;
+  getTabsWithImages(function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      (function() {
+        var url = tabs[i].url;
+        var tabId = tabs[i].id;
 
-				chrome.downloads.download({
-						url: url,
-						conflictAction: "uniquify" // TODO add user-editable setting for what conflict action to use
-					}, function(id) {
-						if (id) {
-							// Download successful
-							$('#links li a[href="' + url + '"]').parent().addClass('done');
-							tabDownloadStatuses[tabId] = true;
-						} else {
-							// Download failed
-							$('#links li a[href="' + url + '"]').parent().addClass('error');
-							tabDownloadStatuses[tabId] = false;
-						}
-					}
-				);
-			})();
-		}
-	});
+        chrome.downloads.download({
+            url: url,
+            conflictAction: "uniquify" // TODO add user-editable setting for what conflict action to use
+          }, function(id) {
+            if (id) {
+              // Download successful
+              $('#links li a[href="' + url + '"]').parent().addClass('done');
+              tabDownloadStatuses[tabId] = true;
+            } else {
+              // Download failed
+              $('#links li a[href="' + url + '"]').parent().addClass('error');
+              tabDownloadStatuses[tabId] = false;
+            }
+          }
+        );
+      })();
+    }
+  });
 }
 
 function closePopup() {
-    window.close();
+  window.close();
 }
 
 function closeDownloadedTabs() {
-	var tabIds = [];
-	for (var id in tabDownloadStatuses) {
-		if (tabDownloadStatuses.hasOwnProperty(id)) {
-			if (tabDownloadStatuses[id] === true) {
-				tabIds.push(parseInt(id));
-			}
-		}
-	}
-	chrome.tabs.remove(tabIds);
+  var tabIds = [];
+  for (var id in tabDownloadStatuses) {
+    if (tabDownloadStatuses.hasOwnProperty(id)) {
+      if (tabDownloadStatuses[id] === true) {
+        tabIds.push(parseInt(id));
+      }
+    }
+  }
+  chrome.tabs.remove(tabIds);
 }
 
 // TODO currently unused
 function timestampedFolderName() {
-	moment().format('YYYY-MM-DD') + "/"
+  moment().format('YYYY-MM-DD') + "/"
 }
 
 // TODO domready?
 window.onload = function() {
-	/* TODO - WIP custom subdirectories in Downloads folder
+  /* TODO - WIP custom subdirectories in Downloads folder
 
-	chrome.downloads.onDeterminingFilename.addListener(function(downloadItem, suggest) {
-		// TODO add "foldername/" before downloadItem.filename to add subdirectory.
-		// but it doesn't guarantee batching all downloads correctly.
-		suggest({
-			filename: downloadItem.filename,
-		});
-	});
-	*/
+  chrome.downloads.onDeterminingFilename.addListener(function(downloadItem, suggest) {
+    // TODO add "foldername/" before downloadItem.filename to add subdirectory.
+    // but it doesn't guarantee batching all downloads correctly.
+    suggest({
+      filename: downloadItem.filename,
+    });
+  });
+  */
 
-	showImageUrls();
+  showImageUrls();
 
-	$('#download').click(downloadImageUrls);
-	$('#dismiss').click(closePopup);
-	$('#close-tabs').click(closeDownloadedTabs);
+  $('#download').click(downloadImageUrls);
+  $('#dismiss').click(closePopup);
+  $('#close-tabs').click(closeDownloadedTabs);
 };
