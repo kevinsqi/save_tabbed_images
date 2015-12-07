c2c715a71583100b7b04e150255f2d63833d470a
Kevin Qi <iqnivek@gmail.com>
2015-12-06 21:53:55 -0500

    Create dist subfolder so node_modules aren't included

--- extension/build.sh
+++ extension/build.sh
@@ -2,6 +2,8 @@
 
 # Borrowed from https://github.com/BinaryMuse/chrome-fast-tab-switcher
 
-mkdir -p js_packaged
+mkdir -p dist/js
 
-./node_modules/.bin/browserify -t reactify -o js_packaged/popup.js js_src/popup.jsx
+./node_modules/.bin/browserify -t reactify -o dist/js/popup.js js/popup.jsx
+
+cp js/background.js dist/js/background.js
