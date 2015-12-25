2ea334f91f6e6346bc7a135e9f1988d60ba25367
Kevin Qi <kqi@patientslikeme.com>
2015-12-24 23:44:27 -0500

    Remove unused build.sh

--- extension/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Borrowed from https://github.com/BinaryMuse/chrome-fast-tab-switcher
-
-mkdir -p dist/js
-
-./node_modules/.bin/browserify -t reactify -o dist/js/popup.js js/popup.jsx
-
-cp js/background.js dist/js/background.js
