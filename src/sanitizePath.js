0fb88f1b4509b5a0ea11375100256ac37b7b01b6
Kevin Qi <iqnivek@gmail.com>
2016-04-24 20:08:46 -0400

    Remove import

--- src/sanitizePath.js
+++ src/sanitizePath.js
@@ -1,5 +1,3 @@
-import sanitize from 'sanitize-filename';
-
 // only allow alphanumeric, dashes, underscores, and spaces
 export default function sanitizePath(path) {
   return path.replace(/[^ _-\w]/, '');
